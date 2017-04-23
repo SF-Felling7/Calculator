a47c06f66f57bdecaabffa05b02d79ab5205896d
Sean Felling <Snfelling7@gmail.com>
2017-04-23 16:05:14 -0500

    started client.js

---
+++ public/scripts/client.js
@@ -0,0 +1,8 @@
+console.log("linked");
+
+
+$(document).ready(onReady);
+
+function Onready() {
+  
+}
---
+++ public/scripts/server.js
@@ -0,0 +1,6 @@
+console.log("src");
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('bodyParser');

981c4f82b19fb47bf133a4de259d3635e6562159
Sean Felling <Snfelling7@gmail.com>
2017-04-23 22:24:41 -0500

    added event listeners

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,5 +4,12 @@ console.log("linked");
 $(document).ready(onReady);
 
 function Onready() {
+  $('#addingButton').on('click', addingButton);
+  $('#subtractingButton').on('click', subtractingButton);
+  $('#multiplyingButton').on('click', multiplyingButton);
+  $('#dividingButton').on('click',multiplyingButton);
+}
+
+function addingButton() {
   
 }
--- public/scripts/server.js
+++ public/scripts/server.js
@@ -4,3 +4,22 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('bodyParser');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Listening at 3000");
+});
